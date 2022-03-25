318e6ff4622df156c33e7f39a8d857e493fb1db2
aniljfin <anil.j@finacus.co.in>
2022-03-25 17:13:33 +0530

    Day 1

--- HTML/scripts/addProduct.js
+++ HTML/scripts/addProduct.js
@@ -3,21 +3,78 @@ var idTxt;
 var nameTxt;
 var priceTxt;
 var descTxt;
-var xhr = new XMLHttpRequest();
+var xhr;
+var productForm;
+var errorMessagePanel;
 
-function save(){
+document.addEventListener("DOMContentLoaded", function(){
 
-    console.log("saving the product");
-
-    // read the data from the inputs
+    console.log("DOMContentLoaded");
     idTxt = document.getElementById("idTxt");
     nameTxt = document.getElementById("nameTxt");
     priceTxt = document.getElementById("priceTxt");
     descTxt = document.getElementById("descTxt");
+    errorMessagePanel = document.getElementById("errorMessagePanel");
+    xhr = new XMLHttpRequest();
+    productForm = document.getElementById("productForm");
+
+    idTxt.addEventListener("invalid", function(){
+
+        var errorMessage = document.createElement("p");
+        errorMessage.appendChild(document.createTextNode("The id is required"));
+        errorMessagePanel.appendChild(errorMessage);
+
+
+    }, false);
+
+    nameTxt.addEventListener("invalid", function(){
+        var errorMessage = document.createElement("p");
+
+        if(nameTxt.validity.valueMissing){
+            errorMessage.appendChild(document.createTextNode("The name is required."));
+        }
+        else if(nameTxt.validity.patternMismatch){
+            errorMessage.appendChild(document.createTextNode("Name has an Incorrect pattern"));
+        }
+
+        
+        errorMessagePanel.appendChild(errorMessage);
+    }, false);
+
+
+    priceTxt.addEventListener("invalid", function(){
+
+        var errorMessage = document.createElement("p");
+        errorMessage.appendChild(document.createTextNode("The price is required and minimum of 10"));
+        errorMessagePanel.appendChild(errorMessage);
+    }, false);
+
+
+}, false);
+
+
+function save(){
+
+    console.log("saving the product");
+    //clear the error panel
+    errorMessagePanel.innerHTML = "";
+
+
+    // read the data from the inputs
+    // idTxt = document.getElementById("idTxt");
+    // nameTxt = document.getElementById("nameTxt");
+    // priceTxt = document.getElementById("priceTxt");
+    // descTxt = document.getElementById("descTxt");
 
 
 
     // validate the inputs
+
+    var isFormValid =  productForm.checkValidity();
+    if(!isFormValid){
+        //alert("Please fill the form.")
+        return;
+    }
 
     //submit(post) to the server
 
@@ -46,12 +103,12 @@ function save(){
 function onStateChange(){
 
     if(xhr.readyState === 4 && xhr.status === 201){
-        
-        idTxt.value = "";
-        nameTxt.value = "";
-        descTxt.value = "";
-        priceTxt.value = "";
 
+        // idTxt.value = "";
+        // nameTxt.value = "";
+        // descTxt.value = "";
+        // priceTxt.value = "";
+        productForm.reset();
         alert("Saved");
     }
 
